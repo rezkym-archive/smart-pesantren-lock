7ca8835df67416e77e12a37dfdae83b9fd1ff877
Rezky <rezkymaulanaa06@gmail.com>
2021-08-09 03:22:55 +0700

    Add Metronic sandbox controller "PagesController"

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PagesController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,10 +14,27 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [PagesController::class, 'index']);
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
+
+
+// Route::get('/', 'PagesController@index');
+
+
+// Demo routes
+Route::get('/datatables', 'PagesController@datatables');
+Route::get('/ktdatatables', 'PagesController@ktDatatables');
+Route::get('/select2', 'PagesController@select2');
+Route::get('/jquerymask', 'PagesController@jQueryMask');
+Route::get('/icons/custom-icons', 'PagesController@customIcons');
+Route::get('/icons/flaticon', 'PagesController@flaticon');
+Route::get('/icons/fontawesome', 'PagesController@fontawesome');
+Route::get('/icons/lineawesome', 'PagesController@lineawesome');
+Route::get('/icons/socicons', 'PagesController@socicons');
+Route::get('/icons/svg', 'PagesController@svg');
+
+// Quick search dummy route to display html elements in search dropdown (header search)
+Route::get('/quick-search', 'PagesController@quickSearch')->name('quick-search');

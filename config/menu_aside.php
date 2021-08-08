ac8200be10b12d578d37a19050163e54d01cc7cf
Rezky <rezkymaulanaa06@gmail.com>
2021-08-09 02:56:44 +0700

    add metronic config

---
+++ config/menu_aside.php
@@ -0,0 +1,51 @@
+<?php
+// Aside menu
+return [
+
+    'items' => [
+        // Dashboard
+        [
+            'title' => 'Beranda',
+            'root' => true,
+            'icon' => 'media/svg/icons/Design/Layers.svg', // or can be 'flaticon-home' or any flaticon-*
+            'page' => '/',
+            'new-tab' => false,
+        ],
+
+        // Custom Section
+        [
+            'section' => 'Custom',
+        ],
+        
+        // Custome Content
+
+        // Layout Section
+        [
+            'section' => 'Layout',
+        ],
+
+        // Layout Content
+
+        
+        
+
+        // CRUD Section
+        [
+            'section' => 'CRUD',
+        ],
+
+        // CRUD Content
+
+        
+
+        // Features Section
+        [
+            'section' => 'Features',
+        ],
+
+        // Features Section
+
+        
+    ]
+
+];

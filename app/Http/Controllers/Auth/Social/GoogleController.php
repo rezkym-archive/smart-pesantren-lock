14ed0742bc3be558699965853f865657c213a098
Rezky <rezkymaulanaa06@gmail.com>
2021-08-26 01:35:50 +0700

    add google authentication

---
+++ app/Http/Controllers/Auth/Social/GoogleController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers\Auth\Social;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Laravel\Socialite\Facades\Socialite;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Exception;
+
+use App\Models\User;
+
+class GoogleController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function redirectToGoogle()
+    {
+        return Socialite::driver('google')->redirect();
+    }
+      
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function handleGoogleCallback()
+    {
+        try {
+    
+            $user = Socialite::driver('google')->user();
+     
+            $finduser = User::where('google_id', $user->id)->first();
+     
+            if($finduser){
+     
+                Auth::login($finduser);
+    
+                return redirect('/');
+     
+            }else{
+                $newUser = User::create([
+                    'name' => $user->name,
+                    'email' => $user->email,
+                    'google_id'=> $user->id,
+                    'password' => Hash::make('tes'),
+                ]);
+                
+                // Default role
+                $newUser->assignRole('student');
+
+                // Attempt login
+                Auth::login($newUser);
+     
+                return redirect('/');
+            }
+    
+        } catch (Exception $e) {
+            dd($e->getMessage());
+        }
+    }
+}

79e709cb7f725fb85a1ab601a6202adfed038405
Rezky <rezkymaulanaa06@gmail.com>
2021-08-26 01:29:22 +0700

    temporary route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Laravel\Socialite\Facades\Socialite;
 use App\Http\Controllers\PagesController;
+use App\Http\Controllers\Auth\Social\GoogleController;
+
+
 
 /*
 |--------------------------------------------------------------------------
@@ -14,7 +18,37 @@ use App\Http\Controllers\PagesController;
 |
 */
 
-Route::get('/', [PagesController::class, 'index']);
+Route::middleware(['role:admin', 'auth:sanctum'])->get('/', [PagesController::class, 'index']);
+
+
+/* Admin Role */
+Route::group([
+       'middleware' => ['auth:sanctum', 'role:admin'],
+       'prefix'     => 'admin',
+       'as'         => 'admin.',
+    ],
+    function () {
+
+        // Dashboard
+        Route::get('/', [\Controller\Admin\HomeController::class, 'create'])->name('home');
+
+        // Data santri route
+        Route::get('data-santri', [\Controller\Admin\Kesantrian\DataSantri::class, 'create'])->name('data-santri');
+
+        // Data izin santri route
+        Route::get('student-permission', [\Controller\Admin\Kesantrian\StudentPermission::class, 'create'])->name('student-permission');
+
+        // Aksi kesantrian route
+    }
+);
+
+
+
+
+
+
+
+
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
@@ -24,17 +58,23 @@ Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
 // Route::get('/', 'PagesController@index');
 
 
+
 // Demo routes
-Route::get('/datatables', 'PagesController@datatables');
-Route::get('/ktdatatables', 'PagesController@ktDatatables');
-Route::get('/select2', 'PagesController@select2');
-Route::get('/jquerymask', 'PagesController@jQueryMask');
-Route::get('/icons/custom-icons', 'PagesController@customIcons');
-Route::get('/icons/flaticon', 'PagesController@flaticon');
-Route::get('/icons/fontawesome', 'PagesController@fontawesome');
-Route::get('/icons/lineawesome', 'PagesController@lineawesome');
-Route::get('/icons/socicons', 'PagesController@socicons');
-Route::get('/icons/svg', 'PagesController@svg');
+Route::get('/datatables', [PagesController::class, 'datatables']);
+Route::get('/ktdatatables', [PagesController::class, 'ktDatatables']);
+Route::get('/select2', [PagesController::class, 'select2']);
+Route::get('/jquerymask', [PagesController::class, 'jQueryMask']);
+Route::get('/icons/custom-icons', [PagesController::class, 'customIcons']);
+Route::get('/icons/flaticon', [PagesController::class, 'flaticon']);
+Route::get('/icons/fontawesome', [PagesController::class, 'fontawesome']);
+Route::get('/icons/lineawesome', [PagesController::class, 'lineawesome']);
+Route::get('/icons/socicons', [PagesController::class, 'socicons']);
+Route::get('/icons/svg', [PagesController::class, 'svg']);
 
 // Quick search dummy route to display html elements in search dropdown (header search)
-Route::get('/quick-search', 'PagesController@quickSearch')->name('quick-search');
+Route::get('/quick-search', [PagesController::class, 'quickSearch'])->name('quick-search');
+
+// Google auth (Laravel socialate)
+/* Route::get('/home', 'HomeController@index')->name('home'); */
+Route::get('auth/google', [GoogleController::class, 'redirectToGoogle'])->name('auth-google');
+Route::get('auth/google/callback', [GoogleController::class, 'handleGoogleCallback'])->name('callback-google');

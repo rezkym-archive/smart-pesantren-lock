d4cfdb0a59bac9fcc98cc1e964aa9a51ff2f3186
Rezky <rezkymaulanaa06@gmail.com>
2021-08-26 01:38:58 +0700

    update guest.blade.php

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -1,24 +1,153 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" {{ Metronic::printAttrs('html') }}
+    {{ Metronic::printClasses('html') }}>
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+<head>
+    <meta charset="utf-8" />
 
-        <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+    {{-- Title Section --}}
+    <title>{{ config('app.name') }} | @yield('title', $page_title ?? '')</title>
 
-        <!-- Styles -->
-        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+    {{-- Meta Data --}}
+    <meta name="description" content="@yield('page_description', $page_description ?? '')" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <!-- Scripts -->
-        <script src="{{ mix('js/app.js') }}" defer></script>
-    </head>
-    <body>
-        <div class="font-sans text-gray-900 antialiased">
-            {{ $slot }}
+    {{-- Favicon --}}
+    <link rel="shortcut icon" href="{{ asset('media/logos/favicon.ico') }}" />
+
+    {{-- Fonts --}}
+    {{ Metronic::getGoogleFontsInclude() }}
+
+    {{ $styles }}
+
+    {{-- Global Theme Styles (used by all pages) --}}
+    @foreach (config('layout.resources.css') as $style_global)
+        <link
+            href="{{ config('layout.self.rtl') ? asset(Metronic::rtlCssPath($style_global)) : asset($style_global) }}"
+            rel="stylesheet" type="text/css" />
+    @endforeach
+
+    {{-- Layout Themes (used by all pages) --}}
+    @foreach (Metronic::initThemes() as $theme)
+        <link href="{{ config('layouts.self.rtl') ? asset(Metronic::rtlCssPath($theme)) : asset($theme) }}"
+            rel="stylesheet" type="text/css" />
+    @endforeach
+
+    {{-- Includable CSS --}}
+    {{-- @yield('styles') --}}
+
+
+</head>
+<!--end::Head-->
+
+<!--begin::Body-->
+
+<body id="kt_body"
+    class="header-fixed header-mobile-fixed subheader-enabled subheader-fixed aside-enabled aside-fixed aside-minimize-hoverable page-loading">
+    <!--begin::Main-->
+    <div class="d-flex flex-column flex-root">
+
+        <!--begin::Login-->
+        <div class="login login-2 login-signin-on d-flex flex-column flex-lg-row flex-column-fluid bg-white"
+            id="kt_login">
+            <!--begin::Aside-->
+            <div class="login-aside order-2 order-lg-1 d-flex flex-row-auto position-relative overflow-hidden">
+                <!--begin: Aside Container-->
+                <div class="d-flex flex-column-fluid flex-column justify-content-between py-9 px-7 py-lg-13 px-lg-35">
+                    <!--begin::Logo-->
+                    {{-- <a href="#" class="text-center pt-2">
+                        <img src="{{ asset('media/logos/logo.png') }}" class="max-h-75px" alt="" />
+                    </a> --}}
+                    <!--end::Logo-->
+                    <!--begin::Aside body-->
+                    <div class="d-flex flex-column-fluid flex-column flex-center">
+
+                        {{-- Content --}}
+                        {{ $slot }}
+
+                    </div>
+                    <!--end::Aside body-->
+                    <!--begin: Aside footer for desktop-->
+                    <div class="text-center">
+                        <button type="button"
+                            class="btn btn-light-primary font-weight-bolder px-8 py-4 my-3 font-size-h6">
+                            <span class="svg-icon svg-icon-md">
+                                <!--begin::Svg Icon | path:assets/media/svg/social-icons/google.svg-->
+                                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20"
+                                    fill="none">
+                                    <path
+                                        d="M19.9895 10.1871C19.9895 9.36767 19.9214 8.76973 19.7742 8.14966H10.1992V11.848H15.8195C15.7062 12.7671 15.0943 14.1512 13.7346 15.0813L13.7155 15.2051L16.7429 17.4969L16.9527 17.5174C18.879 15.7789 19.9895 13.221 19.9895 10.1871Z"
+                                        fill="#4285F4" />
+                                    <path
+                                        d="M10.1993 19.9313C12.9527 19.9313 15.2643 19.0454 16.9527 17.5174L13.7346 15.0813C12.8734 15.6682 11.7176 16.0779 10.1993 16.0779C7.50243 16.0779 5.21352 14.3395 4.39759 11.9366L4.27799 11.9466L1.13003 14.3273L1.08887 14.4391C2.76588 17.6945 6.21061 19.9313 10.1993 19.9313Z"
+                                        fill="#34A853" />
+                                    <path
+                                        d="M4.39748 11.9366C4.18219 11.3166 4.05759 10.6521 4.05759 9.96565C4.05759 9.27909 4.18219 8.61473 4.38615 7.99466L4.38045 7.8626L1.19304 5.44366L1.08875 5.49214C0.397576 6.84305 0.000976562 8.36008 0.000976562 9.96565C0.000976562 11.5712 0.397576 13.0882 1.08875 14.4391L4.39748 11.9366Z"
+                                        fill="#FBBC05" />
+                                    <path
+                                        d="M10.1993 3.85336C12.1142 3.85336 13.406 4.66168 14.1425 5.33717L17.0207 2.59107C15.253 0.985496 12.9527 0 10.1993 0C6.2106 0 2.76588 2.23672 1.08887 5.49214L4.38626 7.99466C5.21352 5.59183 7.50242 3.85336 10.1993 3.85336Z"
+                                        fill="#EB4335" />
+                                </svg>
+                                <!--end::Svg Icon-->
+                            </span>
+                            <a href="{{ route('auth-google') }}">
+								Masuk dengan google
+							</a>
+                        </button>
+                    </div>
+                    <!--end: Aside footer for desktop-->
+                </div>
+                <!--end: Aside Container-->
+            </div>
+            <!--begin::Aside-->
+			
+            {{-- Adjust image to device --}}
+            @if (Agent::isDesktop())
+                <!--begin::Content-->
+                <div class="content order-1 order-lg-2 d-flex flex-column w-100 pb-0"
+                    style="background-color: #B1DCED;">
+                    <!--begin::Title-->
+                    <div
+                        class="d-flex flex-column justify-content-center text-center pt-lg-40 pt-md-5 pt-sm-5 px-lg-0 pt-5 px-7">
+                        <h3 class="display4 font-weight-bolder my-7 text-dark" style="color: #986923;">
+                            {{ env('APP_NAME') }} </h3>
+                        <p class="font-weight-bolder font-size-h2-md font-size-lg text-dark opacity-70">
+                            {{ env('APP_DESCRIPTION') }}
+                        </p>
+                    </div>
+                    <!--end::Title-->
+                    <!--begin::Image-->
+                    <div class="content-img d-flex flex-row-fluid bgi-no-repeat bgi-position-y-bottom bgi-position-x-center"
+                        style="background-image: url({{ asset('media/svg/illustrations/login-visual-2.svg') }});">
+                    </div>
+                    <!--end::Image-->
+                </div>
+                <!--end::Content-->
+            @endif
+
         </div>
-    </body>
+        <!--end::Login-->
+
+    </div>
+    <!--end::Main-->
+
+
+    {{-- Global Config (global config for global JS scripts) --}}
+    <script>
+        var KTAppSettings = {!! json_encode(config('layout.js'), JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES) !!};
+    </script>
+
+    {{-- Global Theme JS Bundle (used by all pages) --}}
+    @foreach (config('layout.resources.js') as $script)
+        <script src="{{ asset($script) }}" type="text/javascript"></script>
+    @endforeach
+
+    {{-- Page Scripts(used by this page) --}}
+    {{ $scripts }}
+    <!--end::Page Scripts-->
+
+</body>
+<!--end::Body-->
+
 </html>

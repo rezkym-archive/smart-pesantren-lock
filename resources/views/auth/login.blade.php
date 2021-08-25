4a8a7e3a5e058398accc641a296a9c861d3589a9
Rezky <rezkymaulanaa06@gmail.com>
2021-08-26 01:43:27 +0700

    update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,48 +1,155 @@
 <x-guest-layout>
-    <x-jet-authentication-card>
-        <x-slot name="logo">
-            <x-jet-authentication-card-logo />
-        </x-slot>
 
-        <x-jet-validation-errors class="mb-4" />
+    <!--begin::Signin-->
+    <div class="login-form login-signin py-11">
 
-        @if (session('status'))
-            <div class="mb-4 font-medium text-sm text-green-600">
-                {{ session('status') }}
-            </div>
-        @endif
+        <!--begin::validation errors-->
+        <x-metronic.validation-errors />
 
-        <form method="POST" action="{{ route('login') }}">
+
+
+        <!--begin::Form-->
+        <form class="form" action="{{ route('login') }}" method="POST" novalidate="novalidate"
+            id="kt_login_signin_form">
+            <!--- begin:Csrf -->
             @csrf
 
-            <div>
-                <x-jet-label for="email" value="{{ __('Email') }}" />
-                <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+            <!--begin::Title-->
+            <div class="text-center pb-8">
+                <h2 class="font-weight-bolder text-dark font-size-h2 font-size-h1-lg"> Masuk</h2>
+                @if (Route::has('register'))
+                    <span class="text-muted font-weight-bold font-size-h4">atau
+                        <a href="" class="text-primary font-weight-bolder" id="kt_login_signup"> Buat Akun</a>
+                    </span>
+                @endif
             </div>
-
-            <div class="mt-4">
-                <x-jet-label for="password" value="{{ __('Password') }}" />
-                <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
+            <!--end::Title-->
+            <!--begin::Form group-->
+            <div class="form-group">
+                <label class="font-size-h6 font-weight-bolder text-dark"> Email</label>
+                <input class="form-control form-control-solid h-auto py-7 px-6 rounded-lg" type="text" name="email"
+                    autocomplete="off" />
             </div>
+            <!--end::Form group-->
+            <!--begin::Form group-->
+            <div class="form-group">
+                <div class="d-flex justify-content-between mt-n5">
+                    <label class="font-size-h6 font-weight-bolder text-dark pt-5"> Kata sandi</label>
+                    @if (Route::has('password.request'))
+                        <a href="javascript:;"
+                            class="text-primary font-size-h6 font-weight-bolder text-hover-primary pt-5"
+                            id="kt_login_forgot">
+                            Lupa kata sandi ?
+                        </a>
+                    @endif
+                </div>
+                <input class="form-control form-control-solid h-auto py-7 px-6 rounded-lg" type="password"
+                    name="password" autocomplete="off" />
+            </div>
+            <!--end::Form group-->
+            <!--begin::Action-->
+            <div class="text-center pt-2">
+                <button id="kt_login_signin_submit" class="btn btn-dark font-weight-bolder font-size-h6 px-8 py-4 my-3">
+                    Masuk
+                </button>
+            </div>
+            <!--end::Action-->
+        </form>
+        <!--end::Form-->
+    </div>
+    <!--end::Signin-->
 
-            <div class="block mt-4">
-                <label for="remember_me" class="flex items-center">
-                    <x-jet-checkbox id="remember_me" name="remember" />
-                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+    <!--begin::Signup-->
+    <div class="login-form login-signup pt-11">
+        <!--begin::Form-->
+        <form class="form" novalidate="novalidate" id="kt_login_signup_form">
+            <!--begin::Title-->
+            <div class="text-center pb-8">
+                <h2 class="font-weight-bolder text-dark font-size-h2 font-size-h1-lg"> Daftar</h2>
+                {{-- <p class="text-muted font-weight-bold font-size-h4"> </p> --}}
+            </div>
+            <!--end::Title-->
+            <!--begin::Form group-->
+            <div class="form-group">
+                <input class="form-control form-control-solid h-auto py-7 px-6 rounded-lg font-size-h6" type="text"
+                    placeholder="Nama Lengkap" name="fullname" autocomplete="off" />
+            </div>
+            <!--end::Form group-->
+            <!--begin::Form group-->
+            <div class="form-group">
+                <input class="form-control form-control-solid h-auto py-7 px-6 rounded-lg font-size-h6" type="email"
+                    placeholder="Email" name="email" autocomplete="off" />
+            </div>
+            <!--end::Form group-->
+            <!--begin::Form group-->
+            <div class="form-group">
+                <input class="form-control form-control-solid h-auto py-7 px-6 rounded-lg font-size-h6" type="password"
+                    placeholder="Kata sandi" name="password" autocomplete="off" />
+            </div>
+            <!--end::Form group-->
+            <!--begin::Form group-->
+            <div class="form-group">
+                <label class="checkbox mb-0">
+                    <input type="checkbox" name="agree" />
+                    <a href="#">
+                        Syarat dan kondisi
+                    </a>
+                    <span></span>
                 </label>
             </div>
-
-            <div class="flex items-center justify-end mt-4">
-                @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
-                    </a>
-                @endif
-
-                <x-jet-button class="ml-4">
-                    {{ __('Log in') }}
-                </x-jet-button>
+            <!--end::Form group-->
+            <!--begin::Form group-->
+            <div class="form-group d-flex flex-wrap flex-center pb-lg-0 pb-3">
+                <button type="button" id="kt_login_signup_submit"
+                    class="btn btn-primary font-weight-bolder font-size-h6 px-8 py-4 my-3 mx-4">Submit</button>
+                <button type="button" id="kt_login_signup_cancel"
+                    class="btn btn-light-primary font-weight-bolder font-size-h6 px-8 py-4 my-3 mx-4">Cancel</button>
             </div>
+            <!--end::Form group-->
         </form>
-    </x-jet-authentication-card>
+        <!--end::Form-->
+    </div>
+    <!--end::Signup-->
+
+    <!--begin::Forgot-->
+    <div class="login-form login-forgot pt-11">
+        <!--begin::Form-->
+        <form class="form" novalidate="novalidate" id="kt_login_forgot_form">
+            <!--begin::Title-->
+            <div class="text-center pb-8">
+                <h2 class="font-weight-bolder text-dark font-size-h2 font-size-h1-lg"> Lupa kata sandi ?</h2>
+                <p class="text-muted font-weight-bold font-size-h4"> Masukkan email kamu.</p>
+            </div>
+            <!--end::Title-->
+            <!--begin::Form group-->
+            <div class="form-group">
+                <input class="form-control form-control-solid h-auto py-7 px-6 rounded-lg font-size-h6" type="email"
+                    placeholder="Email" name="email" autocomplete="off" />
+            </div>
+            <!--end::Form group-->
+            <!--begin::Form group-->
+            <div class="form-group d-flex flex-wrap flex-center pb-lg-0 pb-3">
+                <button type="button" id="kt_login_forgot_submit"
+                    class="btn btn-primary font-weight-bolder font-size-h6 px-8 py-4 my-3 mx-4">Submit</button>
+                <button type="button" id="kt_login_forgot_cancel"
+                    class="btn btn-light-primary font-weight-bolder font-size-h6 px-8 py-4 my-3 mx-4">Cancel</button>
+            </div>
+            <!--end::Form group-->
+        </form>
+        <!--end::Form-->
+    </div>
+    <!--end::Forgot-->
+
+
+    {{-- Styles Section --}}
+    <x-slot name="styles">
+        <link href="{{ asset('css/pages/login/login-2.css') }}" rel="stylesheet" type="text/css" />
+    </x-slot>
+
+    {{-- Scripts Section --}}
+    <x-slot name="scripts">
+        <script src="{{ asset('js/pages/custom/login/login-general.js') }}"></script>
+    </x-slot>
+
+
 </x-guest-layout>

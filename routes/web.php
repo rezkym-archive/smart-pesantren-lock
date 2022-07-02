d9073b35b17bd2c89c6d0415dbed5c00c2ba6dbf
Rezky Maulana <rezkymaulanaa06@gmail.com>
2022-07-02 16:50:56 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\StaterkitController;
+use App\Http\Controllers\LanguageController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', [StaterkitController::class, 'home'])->name('home');
+Route::get('home', [StaterkitController::class, 'home'])->name('home');
+// Route Components
+Route::get('layouts/collapsed-menu', [StaterkitController::class, 'collapsed_menu'])->name('collapsed-menu');
+Route::get('layouts/full', [StaterkitController::class, 'layout_full'])->name('layout-full');
+Route::get('layouts/without-menu', [StaterkitController::class, 'without_menu'])->name('without-menu');
+Route::get('layouts/empty', [StaterkitController::class, 'layout_empty'])->name('layout-empty');
+Route::get('layouts/blank', [StaterkitController::class, 'layout_blank'])->name('layout-blank');
+
+
+// locale Route
+Route::get('lang/{locale}', [LanguageController::class, 'swap']);
